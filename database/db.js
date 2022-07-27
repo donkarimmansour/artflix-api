1e87db7197d80b848edfe96e936615985c62f7df
karim wydadi <don.karimmansour@gmail.com>
2022-07-27 11:15:38 +0100

    hh

--- database/db.js
+++ database/db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
 
 
-const DB_URL = "mongodb://localhost:27017/shop"
-//const DB_URL = "mongodb+srv://admin:admin@cluster0.3av2c.mongodb.net/artflix"
+//const DB_URL = "mongodb://localhost:27017/shop"
+const DB_URL = "mongodb+srv://admin:admin@cluster0.3av2c.mongodb.net/artflix"
 
 function DB(){      
     return mongoose.connect(DB_URL, (err) => { 
